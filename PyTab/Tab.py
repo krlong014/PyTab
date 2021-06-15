749c5ed0b862825fb677f1ea34b5aca6217ff928
Katharine Long <krlong014@gmail.com>
2021-06-14 19:06:24 -0500

    Allow user to indent/unindent explicitly

--- PyTab/Tab.py
+++ PyTab/Tab.py
@@ -1,28 +1,36 @@
 '''! @ brief Tab management.'''
 
 class Tab:
-  '''! Tab is an object for managing tabbed output.
+    '''! Tab is an object for managing tabbed output.
 
-  @param tabsize -- width of tab (number of blank spaces)
-  @param depth -- current tab depth
-  '''
+    @param tabsize -- width of tab (number of blank spaces)
+    @param depth -- current tab depth
+    '''
 
-  tabsize = 2
-  depth = 0
+    tabsize = 2
+    depth = 0
 
-  # Increase the tab depth upon constructing a new object
-  def __init__(self):
-    '''When a Tab constructor is called, the tab depth is increased'''
-    self.depth = Tab.depth
-    Tab.depth += 1
+    # Increase the tab depth upon constructing a new object
+    def __init__(self):
+        '''When a Tab constructor is called, the tab depth is increased'''
+        self.depth = Tab.depth
+        Tab.depth += 1
+
+    def indent(self):
+        self.depth += 1
+        Tab.depth += 1
+
+    def unindent(self):
+        self.depth -= 1
+        Tab.depth -= 1
 
 
-  # The string representation is the specified number of spaces
-  def __str__(self):
-    '''Write a Tab to string.'''
-    return ' ' * (Tab.tabsize*self.depth)
+    # The string representation is the specified number of spaces
+    def __str__(self):
+        '''Write a Tab to string.'''
+        return ' ' * (Tab.tabsize*self.depth)
 
-  # Upon deletion of an object, decrement the tab depth
-  def __del__(self):
-    '''Tab destructor. Upon deletion of an object, decrement the tab depth.'''
-    Tab.depth -= 1
+    # Upon deletion of an object, decrement the tab depth
+    def __del__(self):
+        '''Tab destructor. Upon deletion of an object, decrement the tab depth.'''
+        Tab.depth -= 1
